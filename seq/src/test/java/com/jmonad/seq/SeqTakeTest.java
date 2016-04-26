173a65bc26b55452fee2b402e709a4c1a0354fce
Daniel <wowmegadenis@hotmail.com>
2016-04-26 19:50:53 -0300

    Add take test class

---
+++ seq/src/test/java/com/jmonad/seq/SeqTakeTest.java
@@ -0,0 +1,16 @@
+package com.jmonad.seq;
+
+import org.junit.Test;
+
+public class SeqTakeTest {
+
+    @Test public void takeListElementsTest() {
+        Integer[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Seq<Integer> elements = new Seq<Integer>(numbers);
+
+        assert elements.take(5).toArrayList().toString().equals("[1, 2, 3, 4, 5]");
+        assert elements.take(0).toArrayList().toString().equals("[]");
+        assert elements.take(20).toArrayList().toString().equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+    }
+}
