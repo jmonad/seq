8b87cc3d5e620baec4e934eb9cd3e2a9a378dce6
Daniel <wowmegadenis@hotmail.com>
2016-04-26 22:48:04 -0300

    Split drop function tests

--- seq/src/test/java/com/jmonad/seq/SeqDropTest.java
+++ seq/src/test/java/com/jmonad/seq/SeqDropTest.java
@@ -4,13 +4,22 @@ import org.junit.Test;
 
 public class SeqDropTest {
 
+  private Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  private Seq<Integer> elements = new Seq<Integer>(numbers);
+
   @Test public void dropListElementsTest() {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    Seq<Integer> elements = new Seq<Integer>(numbers);
-
     assert elements.drop(5).toArrayList().toString().equals("[6, 7, 8, 9, 10]");
+  }
+
+  @Test public void dropElementsWithNoAmountTest() {
     assert elements.drop(0).toArrayList().toString().equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+  }
+
+  @Test public void dropElementsWithNegativeAmountTest() {
+    assert elements.drop(-1).toArrayList().toString().equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+  }
+
+  @Test public void dropElementsWithAmountHigherThanListSizeTest() {
     assert elements.drop(20).toArrayList().toString().equals("[]");
   }
 }
