331cd687cec70e43a420805bb595142c3abf5fa6
Daniel <wowmegadenis@hotmail.com>
2016-04-26 22:48:40 -0300

    Split take function tests

--- seq/src/test/java/com/jmonad/seq/SeqTakeTest.java
+++ seq/src/test/java/com/jmonad/seq/SeqTakeTest.java
@@ -4,13 +4,22 @@ import org.junit.Test;
 
 public class SeqTakeTest {
 
+  private Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  private Seq<Integer> elements = new Seq<Integer>(numbers);
+
   @Test public void takeListElementsTest() {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    Seq<Integer> elements = new Seq<Integer>(numbers);
-
     assert elements.take(5).toArrayList().toString().equals("[1, 2, 3, 4, 5]");
+  }
+
+  @Test public void takeElementsWithNoAmountTest() {
     assert elements.take(0).toArrayList().toString().equals("[]");
+  }
+
+  @Test public void takeElementsWithNegativeAmountTest() {
+    assert elements.take(-1).toArrayList().toString().equals("[]");
+  }
+
+  @Test public void takeElementsWithAmountHigherThanListSizeTest() {
     assert elements.take(20).toArrayList().toString().equals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
   }
 }
