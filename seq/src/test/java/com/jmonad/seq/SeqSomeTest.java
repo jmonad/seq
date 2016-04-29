ef69fcd4841f66d626458a7e18d247f1887e0a74
bloder <wowmegadenis@hotmail.com>
2016-04-29 14:15:55 -0300

    Fix checkList problem in seq test class

--- seq/src/test/java/com/jmonad/seq/SeqSomeTest.java
+++ seq/src/test/java/com/jmonad/seq/SeqSomeTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 
 public class SeqSomeTest {
 
-    private Integer[] numbers = {100, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  private Integer[] numbers = {100, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-    private Function trueFunc = new Function<Boolean, Integer>() {
-        @Override public Boolean call(Integer param) {
-            return param > 10;
-        }
-    };
+  private Function trueFunc = new Function<Boolean, Integer>() {
+      @Override public Boolean call(Integer param) {
+          return param > 10;
+      }
+  };
 
-    private Function falseFunc = new Function<Boolean, Integer>() {
-        @Override public Boolean call(Integer param) {
-            return param > 100;
-        }
-    };
+  private Function falseFunc = new Function<Boolean, Integer>() {
+      @Override public Boolean call(Integer param) {
+          return param > 100;
+      }
+  };
 
-    @Test public void expectTrueFromSomeFunctionTest() {
-        assert new Seq<>(numbers).some(trueFunc);
-    }
+  @Test public void expectTrueFromSomeFunctionTest() {
+      assert new Seq<>(numbers).some(trueFunc);
+  }
 
-    @Test public void expectFalseFromSomeFunctionTest() {
-        assert !new Seq<>(numbers).some(falseFunc);
-    }
+  @Test public void expectFalseFromSomeFunctionTest() {
+      assert !new Seq<>(numbers).some(falseFunc);
+  }
 }
